d00350e86766c903586bac80d37d032449463d03
satinP <pedro5satin@gmail.com>
2024-06-04 12:51:05 -0300

    Atividade do dia 23

---
+++ 2024-06-04-resolucao-atividade-edit-todo/pages/todo/index.js
@@ -0,0 +1,82 @@
+const taskKey = '@tasks'
+
+let selectedTaskId = null
+
+// Função para adicionar tarefa
+function addTask(event) {
+  event.preventDefault() // Evita o recarregamento da página
+  const taskId = new Date().getTime()
+  const taskList = document.querySelector('#taskList')
+
+  const form = document.querySelector('#taskForm')
+  const formData = new FormData(form)
+
+  const taskTitle = formData.get('title')
+  const taskDescription = formData.get('description')
+
+  const li = document.createElement('li')
+
+  li.id = taskId
+  li.innerHTML = `
+    <div>
+      <h2>${taskTitle}</h2>
+      <p>${taskDescription}</p>
+    </div>
+    <button title="Editar tarefa" onClick="openEditDialog(${taskId})">✏️</button>
+  `
+
+  taskList.appendChild(li)
+
+  // Salvar tarefas no localStorage
+  const tasks = JSON.parse(localStorage.getItem(taskKey)) || []
+  tasks.push({
+    id: taskId,
+    title: taskTitle,
+    description: taskDescription,
+  })
+  localStorage.setItem(taskKey, JSON.stringify(tasks))
+
+  form.reset()
+}
+
+function openEditDialog(taskId) {
+  const tasks = JSON.parse(localStorage.getItem(taskKey)) || []
+
+  selectedTaskId = tasks.findIndex((task) => task.id === taskId)
+  const task = tasks[selectedTaskId]
+
+  const dialog = document.querySelector('dialog')
+
+  const editTitle = document.querySelector('#editTaskForm #title')
+  const editDescription = document.querySelector('#editTaskForm #description')
+
+  editTitle.value = task.title
+  editDescription.value = task.description
+
+  dialog.showModal()
+}
+
+function closeDialog() {
+  const dialog = document.querySelector('dialog')
+  dialog.close()
+}
+
+// Carregar tarefas do localStorage ao recarregar a página
+window.addEventListener('DOMContentLoaded', () => {
+  const tasks = JSON.parse(localStorage.getItem(taskKey)) || []
+  const taskList = document.querySelector('#taskList')
+
+  taskList.innerHTML = tasks
+    .map(
+      (task) => `
+      <li id='id-${task.id}'>
+        <div>
+          <h2>${task.title}</h2>
+          <p>${task.description}</p>
+        </div>
+        <button title="Editar tarefa" onClick="openEditDialog(${task.id})">✏️</button>
+      </li>
+    `
+    )
+    .join('')
+})
